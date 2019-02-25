e891648e3af062eae3cd9ec08e054f9a620c5417
Vlad Tataritsky <tataritskii@bk.ru>
2019-02-25 23:53:28 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var arr = bracketsConfig.join('').replace(/,/g, '');
+    var newStr = str + arr;
+    var newArr = newStr.split('');
+//    {-1     }-2     (-3     )-4     [-5    ]-6      |-7
+    var count1 = 0;
+    var count2 = 0;
+    var count3 = 0;
+    var count4 = 0;
+    var count5 = 0;
+    var count6 = 0;
+    var count7 = 0;
+
+    for (var i = 0; i < newArr.length; i++) {
+        if (newArr[i] == "{") {
+            count1++;
+        } else if (newArr[i] == "}") {
+            count2++;
+        } else if (newArr[i] == "(") {
+            count3++;
+        } else if (newArr[i] == ")") {
+            count4++;
+        } else if (newArr[i] == "[") {
+            count5++;
+        } else if (newArr[i] == "]") {
+            count6++;
+        } else if (newArr[i] == "|") {
+            count7++;
+        }
+    }
+    if (count1 == count2 && count3 == count4 && count5 == count6 && count7 % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+
 }
