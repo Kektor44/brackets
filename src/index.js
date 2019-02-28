885ca73dd55acb73cef71ffd28f29aff743619fd
Vlad Tataritsky <tataritskii@bk.ru>
2019-02-28 13:47:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-
-    var arr = bracketsConfig.join('').replace(/,/g, '');
-    var newStr = str + arr;
-    var newArr = newStr.split('');
-//    {-1     }-2     (-3     )-4     [-5    ]-6      |-7
-    var count1 = 0;
-    var count2 = 0;
-    var count3 = 0;
-    var count4 = 0;
-    var count5 = 0;
-    var count6 = 0;
-    var count7 = 0;
-
-    for (var i = 0; i < newArr.length; i++) {
-        if (newArr[i] == "{" && newArr[i + 1] == "}" || newArr[i] == "(" && newArr[i + 1] == ")" || newArr[i] == "[" && newArr[i + 1] == "]" || newArr[i] == "|" && newArr[i + 1] == "|") {
-            newArr.splice(i, 2);
-            i=i-2;
-
-        }
+    for(var i=0; i < Math.pow(bracketsConfig.length, 2); i++) {
+        bracketsConfig.forEach(function(bracketConfig) {
+            var matchedStr = bracketConfig[0] + bracketConfig[1];
+            while(str.contains(matchedStr)) {
+                str = str.replace(matchedStr, '');
+            }
+        });
     }
-    if (newArr.length == 0) {
-        return true;
-    } else {
-        return false;
-    }
-
-
+    return str == '';
+}
+String.prototype.contains = function(it) { return this.indexOf(it) != -1;
 }
